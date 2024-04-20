380c3e32fbb42a870d4b7d38040106ab8a88d94b
E-jiwoo <livei07@naver.com>
2024-04-21 00:04:00 +0900

    add :: tag - 분위기, 키워드 추가하는 태그 기능

--- src/pages/Create/create.js
+++ src/pages/Create/create.js
@@ -1,19 +1,103 @@
 import React from "react";
+import { useState } from "react";
 import * as S from "./style";
 import logo from "../../assets/logo.svg";
 import back from "../../assets/back.svg";
 
-const create = () => {
+const Create = () => {
+  const initialTags = ["부드러운", "절망스러운", "따사로운"];
+  const initialTags2 = ["남친", "여사친", "사랑", "행복"];
+
+  const [tags1, setTags1] = useState(initialTags);
+  const [tags2, setTags2] = useState(initialTags2);
+
+  const removeTags1 = (indexToRemove) => {
+    const filter = tags1.filter((el, index) => index !== indexToRemove);
+    setTags1(filter);
+  };
+
+  const removeTags2 = (indexToRemove) => {
+    const filter = tags2.filter((el, index) => index !== indexToRemove);
+    setTags2(filter);
+  };
+
+  const addTags1 = (event) => {
+    const inputVal = event.target.value;
+    if (event.key === "Enter" && inputVal !== "" && !tags1.includes(inputVal)) {
+      setTags1([...tags1, inputVal]);
+      event.target.value = "";
+    }
+  };
+
+  const addTags2 = (event) => {
+    const inputVal = event.target.value;
+    if (event.key === "Enter" && inputVal !== "" && !tags2.includes(inputVal)) {
+      setTags2([...tags2, inputVal]);
+      event.target.value = "";
+    }
+  };
+
   return (
     <>
       <S.container>
         <S.logo src={logo} alt="logo" />
         <S.back src={back} alt="back" />
-        <S.title>내 Playlist</S.title>
-        <S.StoryBox type="text" placeholder="사연을 적어 주세요" />
+        <S.title>노래 생성</S.title>
+        <S.StoryBox placeholder="사연을 적어 주세요" />
+        <S.atmosphere>분위기</S.atmosphere>
+        <S.TagsInput>
+          <ul id="tags1">
+            {tags1.map((tag, index) => (
+              <li key={index} className="tag">
+                <span className="tag-title">{tag}</span>
+                <span
+                  className="tag-close-icon"
+                  onClick={() => removeTags1(index)}
+                >
+                  x
+                </span>
+              </li>
+            ))}
+          </ul>
+          <input
+            className="tag-input"
+            type="textarea"
+            onKeyUp={(e) => {
+              addTags1(e);
+            }}
+            placeholder="태그를 입력해주세요"
+          />
+        </S.TagsInput>
+        <S.keyword>키워드</S.keyword>
+        <S.TagsInput style={{ marginTop: "40px" }}>
+          <ul id="tags2">
+            {tags2.map((tag, index) => (
+              <li key={index} className="tag">
+                <span className="tag-title">{tag}</span>
+                <span
+                  className="tag-close-icon"
+                  onClick={() => removeTags2(index)}
+                >
+                  x
+                </span>
+              </li>
+            ))}
+          </ul>
+          <input
+            className="tag-input"
+            type="textarea"
+            onKeyUp={(e) => {
+              addTags2(e);
+            }}
+            placeholder="태그를 입력해주세요"
+          />
+        </S.TagsInput>
+        <S.add>추가 요구사항</S.add>
+        <S.AddBox placeholder="추가 요구사항을 적어 주세요" />
+        <S.CreateBtn>생성하기</S.CreateBtn>
       </S.container>
     </>
   );
 };
 
-export default create;
+export default Create;
--- src/pages/Create/style.js
+++ src/pages/Create/style.js
@@ -24,12 +24,153 @@ export const title = styled.h2`
   font-size: 20px;
 `;
 
-export const StoryBox = styled.input`
+export const StoryBox = styled.textarea`
   position: relative;
   top: 70px;
   left: 25px;
   background: #e7e7e7;
   border: none;
-  width: 335px;
+  width: 305px;
   height: 260px;
+  padding-top: 13px;
+  padding-bottom: 13px;
+  padding-left: 10px;
+  padding-right: 10px;
+  white-space: pre-wrap;
+  font-size: 16px;
+
+  &::placeholder {
+    position: absolute;
+    top: 13px;
+    left: 10px;
+    color: #ff6767;
+    font-size: 16px;
+  }
+`;
+
+export const atmosphere = styled.div`
+  position: relative;
+  left: 25px;
+  top: 100px;
+  font-size: 20px;
+  font-size: 18px;
+  font-weight: 400;
+`;
+
+export const keyword = styled.div`
+  position: relative;
+  left: 25px;
+  top: 140px;
+  font-size: 20px;
+  font-size: 18px;
+  font-weight: 400;
+`;
+
+export const TagsInput = styled.div`
+  position: relative;
+  top: 115px;
+  left: 25px;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  min-height: 30px;
+  width: 305px;
+  padding: 8px;
+  border: 1px solid #ff6767;
+  border-radius: 6px;
+
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0;
+    margin: 8px 0 0 0;
+
+    > .tag {
+      width: auto;
+      height: 32px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #ff6767;
+      padding: 0 8px;
+      font-size: 14px;
+      list-style: none;
+      border-radius: 6px;
+      margin: 0 8px 8px 0;
+      background: rgb(242, 243, 244);
+      border-radius: 15px;
+
+      > .tag-close-icon {
+        display: block;
+        width: 16px;
+        height: 16px;
+        line-height: 16px;
+        text-align: center;
+        font-size: 14px;
+        margin-left: 8px;
+        color: #ff6767;
+        border-radius: 50%;
+        background: #fff;
+        cursor: pointer;
+      }
+    }
+  }
+
+  > input {
+    flex: 1;
+    border: none;
+    height: 30px;
+    font-size: 14px;
+    padding: 5px;
+    :focus {
+      outline: transparent;
+    }
+  }
+`;
+
+export const add = styled.div`
+  position: relative;
+  left: 25px;
+  top: 140px;
+  font-size: 20px;
+  font-size: 18px;
+  font-weight: 400;
+`;
+
+export const AddBox = styled.textarea`
+  position: relative;
+  top: 155px;
+  left: 25px;
+  background: #e7e7e7;
+  border: none;
+  width: 305px;
+  height: 85px;
+  padding-top: 13px;
+  padding-bottom: 13px;
+  padding-left: 10px;
+  padding-right: 10px;
+  white-space: pre-wrap;
+  font-size: 16px;
+
+  &::placeholder {
+    position: absolute;
+    top: 13px;
+    left: 10px;
+    color: #ff6767;
+    font-size: 16px;
+  }
+`;
+
+export const CreateBtn = styled.button`
+  position: fixed;
+  left: 25px;
+  bottom: 22px;
+  padding: 17px 128px;
+  background-color: #ff6767;
+  border: none;
+  border-radius: 6px;
+  color: white;
+  color: #fff5f5;
+  font-size: 20px;
+  font-weight: 700;
 `;
